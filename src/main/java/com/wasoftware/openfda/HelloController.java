47b39601ce5fb91d6b3a016a04c62b49bd483c85
tyipwsi <tyip@wasoftware.com>
2015-06-22 16:41:21 -0400

    by yip, login module

--- src/main/java/com/wasoftware/openfda/HelloController.java
+++ src/main/java/com/wasoftware/openfda/HelloController.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String gotoLogin(ModelMap model) {
+		//model.addAttribute("message", "Hello world!");
+		return "login";
+	}
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello World!");
+		model.addAttribute("message", "Hello world!");
 		return "hello";
 	}
 }
