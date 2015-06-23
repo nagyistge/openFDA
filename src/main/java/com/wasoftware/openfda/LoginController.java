af36fcb7946e3d3b685648029f62847e3f878ab3
tyipwsi <tyip@wasoftware.com>
2015-06-23 16:32:23 -0400

    rename

---
+++ src/main/java/com/wasoftware/openfda/LoginController.java
@@ -0,0 +1,25 @@
+package com.wasoftware.openfda;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+@Controller
+public class LoginController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String gotoLogin(ModelMap model) {
+		//model.addAttribute("message", "Hello world!");
+		return "login";
+	}
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		System.out.println("aaa");
+		model.addAttribute("message", "Hello world!");
+		return "hello";
+	}
+}
