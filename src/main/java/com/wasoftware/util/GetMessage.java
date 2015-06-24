feb85b58e5b7f9bb25759aa50a163c9aaf6bae00
tyipwsi <tyip@wasoftware.com>
2015-06-24 10:15:24 -0400

    new class added

---
+++ src/main/java/com/wasoftware/util/GetMessage.java
@@ -0,0 +1,18 @@
+package com.wasoftware.util;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import org.springframework.context.i18n.LocaleContextHolder;
+/**
+ * Created by yipty on 6/24/2015.
+ */
+public class GetMessage {
+    public static String getMessage (String inputMessageCode){
+        String messageStr = "";
+        //this is only work for spring framework
+        Locale locale = LocaleContextHolder.getLocale();
+        ResourceBundle messages = ResourceBundle.getBundle("messages", locale);
+        messageStr = messages.getString(inputMessageCode);
+        return messageStr;
+    }
+}
