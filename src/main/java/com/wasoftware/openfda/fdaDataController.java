2cda8c6d9f5d055e85d911f16e74e89508b9dce1
tyipwsi <tyip@wasoftware.com>
2015-06-24 10:23:01 -0400

    fda controller changed

--- src/main/java/com/wasoftware/openfda/fdaDataController.java
+++ src/main/java/com/wasoftware/openfda/fdaDataController.java
@@ -8,15 +8,8 @@ import com.wasoftware.openfda.restclient.drug.*;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.json.simple.parser.ParseException;
-import java.io.*;
-
-//javax.json.JsonArray;
-//import javax.json.JsonObject;
-
+import com.wasoftware.util.GetMessage;
 
 /**
  * Created by yipty on 6/22/2015.
@@ -25,51 +18,45 @@ import java.io.*;
 @Controller
 public class FdaDataController {
     @RequestMapping(value = "/fdaData", method = RequestMethod.GET)
-    public String fdadata(ModelMap model) {
+    public String fdaData(ModelMap model) {
         return "fdaData";
     }
     @RequestMapping(value = "/fdaData", method = RequestMethod.POST)
     public String getFdaData(ModelMap model,
-                             @RequestParam(value = "fromDate") String fromDate,
-                             @RequestParam(value = "toDate") String toDate
+                             @RequestParam(value = "fromDate",defaultValue="") String fromDate,
+                             @RequestParam(value = "toDate",defaultValue="") String toDate
                              ) {
-        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxx");
-        System.out.println(fromDate);
-        System.out.println(toDate);
-        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxx");
-
+        String message="";
         if (fromDate.length() > 0 && toDate.length() > 0) {
             adverseEvent adverseevent = new adverseEvent();
-            //String jsonResult = adverseevent.getAdverseEventCountByDate("20040101", "20160101");
-            System.out.println("fd"+ fromDate);
-            System.out.println("td"+ toDate);
             fromDate = reformatDate(fromDate);
             toDate = reformatDate(toDate);
-            String jsonResult = adverseevent.getAdverseEventCountByDate(fromDate, toDate);
+            String jsonResult = "";
             JSONArray jsonArrayResult;
             jsonArrayResult = new JSONArray();
-            try {
-                JSONParser jsonParser = new JSONParser();
+            JSONParser jsonParser = new JSONParser();
+            try{
+                jsonResult = adverseevent.getAdverseEventCountByDate(fromDate, toDate);
+                System.out.println(jsonResult.toString());
                 Object object = jsonParser.parse(jsonResult);
                 JSONObject jsonObject = (JSONObject) object;
                 jsonArrayResult = (JSONArray) jsonObject.get("results");
-            } catch (Exception e) {
+                model.addAttribute("fdaResultSet", jsonArrayResult.toString());
+            }catch(Exception e){
                 System.out.println(e.toString());
+                message = GetMessage.getMessage("fdaData.nodata");
             }
-            //return new ModelAndView("listUsers", "users", users);
 
             model.addAttribute("fdaResultSet", jsonArrayResult.toString());
         }
+        model.addAttribute("message", message);
         return "fdaData";
     }
     public String reformatDate(String unformattedDate){
-        String year = unformattedDate.substring(6, 9);
-        String month = unformattedDate.substring(0, 1);
-        String day = unformattedDate.substring(3, 4);
-        System.out.println("y"+year);
-        System.out.println("m"+month);
-        System.out.println("d"+day);
-        String formattedDate = month + "/" + day + "/" + year;
+        String year = unformattedDate.substring(6, 10);
+        String month = unformattedDate.substring(0, 2);
+        String day = unformattedDate.substring(3, 5);
+        String formattedDate = year + month + day;
         return formattedDate;
     }
 }
