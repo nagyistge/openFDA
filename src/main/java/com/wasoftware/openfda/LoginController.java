2ef77ffa6270a968ccf9b82188110a5c4a0f21d7
tyipwsi <tyip@wasoftware.com>
2015-06-29 17:10:25 -0400

    changed

--- src/main/java/com/wasoftware/openfda/LoginController.java
+++ src/main/java/com/wasoftware/openfda/LoginController.java
@@ -5,19 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.LinkedHashMap;
-import java.util.List;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = {"/","login"}, method = RequestMethod.GET)
 
-	public String gotoLogin(ModelMap model) {
-		return "login";
-	}
+	public String gotoLogin() {return "login";}
 
 	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 
@@ -32,8 +26,7 @@ public class LoginController {
 
 	//for 404 access denied page
 	@RequestMapping(value = "/404", method = RequestMethod.GET)
-	public String accesssDenied(ModelMap model) {
-		//model.addAttribute("message", "Hello world!");
+	public String accesssDenied() {
 		return "login";
 	}
 }
